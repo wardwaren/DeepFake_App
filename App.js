8c5cf4fe4d09d7d075c504e9bf7ad3e7afff2abe
wardwaren <wardwaren@gmail.com>
2020-09-24 23:22:31 +0600

    Small experiments with react native

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { View, Text, Image, ScrollView, TextInput, Button } from "react-native";
 
-export default function App() {
+const getFullName = (name = "", sname = "", tname = "") => {
+  return name + " " + sname + " " + tname;
+};
+
+const App = (props) => {
+  const [isActive, setIsActive] = useState(true);
+  const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView>
+      <Text>
+        Hello, I am {getFullName(props.name, props.sname, props.tname)} and I am{" "}
+        {text} years old
+      </Text>
+      <View>
+        <Text>{!isActive ? "Currently not active" : "Currently active"}</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        placeholder="Enter your age"
+        defaultValue={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <Button
+        onPress={() => {
+          if (isActive) {
+            setIsActive(false);
+          } else {
+            setIsActive(true);
+          }
+        }}
+        title={isActive ? "I am active" : "I am not active"}
+      />
+    </ScrollView>
+  );
+};
+
+const Cafe = () => {
+  return (
+    <View>
+      <Text>Parent func</Text>
+      <App name="Alisher" sname="Sultanov" tname="Tolimbekovich" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Cafe;
